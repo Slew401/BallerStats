dd4681e07fe0d0624a0c46d6c3c9b1abb0e6a219
Slew401 <nv20676@bristol.ac.uk>
2021-10-11 23:17:02 +0100

    Completed Navbar

--- source-code/src/components/Navigation.js
+++ source-code/src/components/Navigation.js
@@ -1,14 +1,43 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import {Switch, Link, Route} from "react-router-dom"
+import {Home, PlayerFinder, TeamFinder, Glossary} from "./index.js"
+
 import ball from './basketball-ball-solid.svg'
+
+
+
 
 const Navigation = () => {
     return (
         <div>
-            <Navbar bg = "bball">
+            <Navbar bg="bball" expand="lg">
                 <img className = "ball-image" src = {ball} alt = "logo"></img>
-                <span><b>BALLERSTATS</b></span>
+                <span><Navbar.Brand href="#home"><b>BUCKETSTATS</b></Navbar.Brand></span>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <Nav.Link as={Link} to = {"/Home"}>Home</Nav.Link>
+                        <Nav.Link as={Link} to = {"/PlayerFinder"}>Player Finder</Nav.Link>
+                        <Nav.Link as={Link} to = {"/TeamFinder"}>TeamFinder</Nav.Link>
+                        <Nav.Link as={Link} to = {"/Glossary"}>Glossary</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>            
             </Navbar>
+            <Switch>
+                <Route path = "/Home">
+                    <Home/>
+                </Route>
+                <Route path = "/PlayerFinder">
+                    <PlayerFinder/>
+                </Route>
+                <Route path = "/TeamFinder">
+                    <TeamFinder/>
+                </Route>
+                <Route path = "/Glossary">
+                    <Glossary/>
+                </Route>
+            </Switch>
         </div>
     )
 }
---
+++ source-code/src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Home} from "./Home.js";
+export {default as PlayerFinder} from "./PlayerFinder.js";
+export {default as TeamFinder} from "./TeamFinder.js";
+export {default as Glossary} from "./Glossary.js";
+export {default as Navigation} from "./Navigation.js";
+
